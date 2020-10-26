6b20233c8525d1a4c4da8daea73d39cf7aabc87b
Omar6741 <70788409+Omar6741@users.noreply.github.com>
2020-10-26 15:42:05 -0500

    Number 2 on Rithm's Asynchronous Function Problem Set.

--- app.js
+++ app.js
@@ -4,3 +4,30 @@ async function get_fav(num) {
     let fav = await axios.get(`${BASE_URL}${num}/trivia?json`)
     return fav.data.text
 }
+
+// (2) (a)
+
+async function getData(num1,num2){
+    
+    let p1 = axios.get(`${BASE_URL}${num1}/trivia?json`);
+    let p2 = axios.get(`${BASE_URL}${num2}/trivia?json`);
+         
+    let results = [await p1, await p2];
+
+    return results;
+}
+
+// (b)
+
+async function getDataOnArray(num1, num2){
+
+    let response = await axios.get(`${BASE_URL}${[num1,num2]}/trivia?json`);
+
+    return response.data;
+
+}
+
+
+
+
+
